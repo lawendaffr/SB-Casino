67867a5d39bb0e9859b1d0b8fc42e97e915f1834
Poloniumlel <maciek.creeper510@gmail.com>
2026-09-12 12:22:00 +0200

    Implement JWT token check on authorized endpoints

---
+++ kasynobackend/src/main/java/me/polonium/kasynobackend/auth/JwtAuthenthicationFilter.java
@@ -0,0 +1,67 @@
+package me.polonium.kasynobackend.auth;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import me.polonium.kasynobackend.entity.User;
+import me.polonium.kasynobackend.repository.UserRepository;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class JwtAuthenthicationFilter extends OncePerRequestFilter   {
+
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    public JwtAuthenthicationFilter(JwtService jwtService, UserRepository userRepository) {
+        this.jwtService = jwtService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorization = request.getHeader("Authorization");
+
+        if(authorization == null ||
+        !authorization.startsWith("Bearer ")) {
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        String token = authorization.substring(7);
+        if(!jwtService.isValid(token)) {
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        Long userId = jwtService.extractUserId(token);
+
+        User user = userRepository.findById(userId).orElse(null);
+        if(user == null) {
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken authentication =
+                new UsernamePasswordAuthenticationToken(
+                        user.getId(),
+                        null,
+                        List.of(new SimpleGrantedAuthority("ROLE_USER"))
+                );
+
+
+        SecurityContextHolder
+                .getContext()
+                .setAuthentication(authentication);
+        filterChain.doFilter(request,response);
+    }
+}
--- kasynobackend/src/main/java/me/polonium/kasynobackend/config/SecurityConfig.java
+++ kasynobackend/src/main/java/me/polonium/kasynobackend/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package me.polonium.kasynobackend.config;
 
+import jakarta.servlet.http.HttpServletResponse;
+import me.polonium.kasynobackend.auth.JwtAuthenthicationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,9 +9,17 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthenthicationFilter jwtAuthenthicationFilter;
+
+    public SecurityConfig(JwtAuthenthicationFilter jwtAuthenthicationFilter) {
+        this.jwtAuthenthicationFilter = jwtAuthenthicationFilter;
+    }
+
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -17,11 +27,20 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(
+                                ((request, response, authException) ->
+                                        response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                        ))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/test/**").permitAll()
                         .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated()
+                )
+                .addFilterBefore(
+                        jwtAuthenthicationFilter,
+                        UsernamePasswordAuthenticationFilter.class
                 );
         return http.build();
     }
