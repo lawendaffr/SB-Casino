84673568065e3e4c6d85dfb5ce079724322d8477
Poloniumlel <maciek.creeper510@gmail.com>
2026-09-13 20:47:33 +0200

    Add auth request body validation

--- kasynobackend/src/main/java/me/polonium/kasynobackend/auth/dto/LoginRequest.java
+++ kasynobackend/src/main/java/me/polonium/kasynobackend/auth/dto/LoginRequest.java
@@ -1,7 +1,11 @@
 package me.polonium.kasynobackend.auth.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record LoginRequest(
+        @NotBlank
         String username,
+        @NotBlank
         String password
 ) {
 }
